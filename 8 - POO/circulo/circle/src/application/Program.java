ef26f4a2ce8c2848eb7a6e615ced7510745ce575
Gustavo Marcal <eletronica1507@gmail.com>
2024-12-23 23:57:17 -0300

    circle exercise V2

--- 8 - POO/circulo/circle/src/application/Program.java
+++ 8 - POO/circulo/circle/src/application/Program.java
@@ -1,37 +1,31 @@
 package application;
 
+import util.Calculator;
+
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
-
-    public static final double PI = 3.14159;
 
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
         Scanner sc  = new Scanner(System.in);
 
+        Calculator calc = new Calculator();
+
         System.out.print("Enter radius: ");
         double radius = sc.nextDouble();
 
-        double c = circunference(radius);
-        double v = volume(radius);
+        double c = calc.circumference(radius);
+        double v = calc.volume(radius);
 
-        System.out.printf("Circunference: %.2f%n", c);
+        System.out.printf("Circumference: %.2f%n", c);
         System.out.printf("Volume: %.2f%n", v);
-        System.out.printf("PI Value: %.4f%n", PI);
+        System.out.printf("PI Value: %.2f%n", calc.PI);
 
         sc.close();
 
-    }
-
-    public static double circunference(double radius){
-        return 2.0 * PI * radius;
-    }
-
-   public static double volume(double radius){
-        return 4.0 * PI * radius * radius * radius / 3.0;
     }
 
 }
