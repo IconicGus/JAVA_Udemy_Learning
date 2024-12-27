693c8769808883ce33bde08e0949e2edab606978
Gustavo Marcal <eletronica1507@gmail.com>
2024-12-26 21:43:09 -0300

    playing with UPCASTING and DOWNCASTING

--- 13 - Heranca Polimorfismo/Heranca1/src/application/Program.java
+++ 13 - Heranca Polimorfismo/Heranca1/src/application/Program.java
@@ -1,9 +1,39 @@
 package application;
 
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
 public class Program {
 
     public static void main(String[] args) {
 
+        Account acc = new Account(0.0, "Alex", 1001);
+        BusinessAccount bacc = new BusinessAccount(0.0, "Maria", 1002, 500.0);
+
+        // UPCASTING
+        // atribuir um objeto de uma subclasse para uma variavel da superclasse:
+
+        Account acc1 = bacc;
+        Account acc2 = new BusinessAccount(0.0, "Bob", 1003, 200.0);
+        Account acc3 = new SavingsAccount(0.0, "Anna", 1004, 0.01);
+
+        // DOWNCASTING
+
+        BusinessAccount acc4 = (BusinessAccount)acc2;
+
+        // BusinessAccount acc5 = (BusinessAccount)acc3; -> não pode fazer o casting por acc2 não ser uma instancia de acc4
+
+        // Sempre melhor verificar se uma é instancia da outra antes de fazer o casting!
+        if (acc3 instanceof BusinessAccount){
+            BusinessAccount acc5 = (BusinessAccount) acc3;
+            acc5.loan(200.0);
+        }
+
+        if (acc3 instanceof SavingsAccount){
+            SavingsAccount acc5 = (SavingsAccount) acc3;
+            acc5.updateBalance();
+        }
     }
 
 }
