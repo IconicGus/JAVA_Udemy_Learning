df10786027ebf4cdd50516ce5a21323ac5218e93
Gustavo Marcal <eletronica1507@gmail.com>
2024-12-30 14:42:04 -0300

    playing with files

--- 15 - working with files/try-with-resourses/src/Main.java
+++ 15 - working with files/try-with-resourses/src/Main.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.Buffer;
 import java.util.Scanner;
 
@@ -23,5 +20,45 @@ public class Main {
         catch (IOException e) {
             System.out.println(e.getMessage());
         }
+
+        writeFile(path);
+
+    }
+
+    public static void writeFile(String path) {
+
+        String[] lines = new String[] { "Good morning", "Good afternoon", "Good night" };
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void playingWithFolders() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a folder path: ");
+        String strPath = sc.nextLine();
+        File path = new File(strPath);
+        File[] folders = path.listFiles(File::isDirectory);
+        System.out.println("FOLDERS:");
+        
+        for (File folder : folders) {
+            System.out.println(folder);
+        }
+        File[] files = path.listFiles(File::isFile);
+        System.out.println("FILES:");
+        for (File file : files) {
+            System.out.println(file);
+        }
+        boolean success = new File(strPath + "\\subdir").mkdir();
+        System.out.println("Directory created successfully: " + success);
+        sc.close();
+
     }
 }
