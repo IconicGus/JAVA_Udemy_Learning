5de3fc1c6cb5f5e908e601da07f0939732dbd71d
Gustavo Marcal <eletronica1507@gmail.com>
2024-12-30 14:19:29 -0300

    bad way to read a file files

--- 15 - working with files/readingFiles/src/Main.java
+++ 15 - working with files/readingFiles/src/Main.java
@@ -1,26 +1,45 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.Buffer;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         File file = new File("C:\\Users\\easmrgu\\OneDrive - Ericsson\\Desktop\\JAVA - Learning\\udemy\\15 - working with files\\in.txt");
-        Scanner sc = null;
+        FileReader fr = null;
+        BufferedReader br = null;
 
         try {
-            sc = new Scanner(file);
-            while (sc.hasNextLine()) {
-                System.out.println(sc.nextLine());
+            fr = new FileReader(file);
+            br = new BufferedReader(fr);
+
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
             }
+
         }
         catch (IOException e) {
             System.out.println(e.getMessage());
         }
         finally {
-            if (sc != null) {
-                sc.close();
+            try {
+                if (br != null) {
+                    br.close();
+                }
+                if (fr != null) {
+                    fr.close();
+                }
             }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
